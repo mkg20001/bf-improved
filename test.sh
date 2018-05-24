25f5e8ae14c70328b65fde56874f12f60ef7b693
Maciej Krüger <mkg20001@gmail.com>
2018-05-24 16:07:09 +0200

    Update tests

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ CLI=(node ./src/cli.js)
 
 set -ex
 
-"${CLI[@]}" bfm example.js example.bf --debug
+"${CLI[@]}" bfm example.bfm example.bf --debug
 "${CLI[@]}" bf example.bf
+"${CLI[@]}" bftoc example.bf example.c
+gcc example.c -o example.out
+./example.out
